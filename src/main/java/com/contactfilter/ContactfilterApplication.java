db1f435679f9d3fbcf2ecd41da7fc2029bee7e08
AS <litvas13@gmail.com>
2018-09-12 18:58:57 +0300

    generated project using start.spring.io

---
+++ src/main/java/com/contactfilter/ContactfilterApplication.java
@@ -0,0 +1,12 @@
+package com.contactfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactfilterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/contactfilter/ContactfilterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.contactfilter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactfilterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
